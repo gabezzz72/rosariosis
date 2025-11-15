2a6cd50bbd0fa46b8a86fd1c42c3b18405880489
gabezzz72 <gabez@mindaccel.org>
2025-11-15 13:46:03 -0600

    Update Assessments.php

--- modules/Assessments/Assessments.php
+++ modules/Assessments/Assessments.php
@@ -9,10 +9,10 @@
  */
 
 // Main entry point.
-// FIX: Use relative paths to go up two directories to the webroot.
-require_once '../../ProgramFunctions/DrawHeader.f.php';
-require_once '../../ProgramFunctions/program_init.f.php';
-require_once '../../ProgramFunctions/SearchForm.f.php';
+// FIX: Paths must be relative to the webroot (Modules.php), not the module directory.
+require_once 'ProgramFunctions/DrawHeader.f.php';
+require_once 'ProgramFunctions/program_init.f.php';
+require_once 'ProgramFunctions/SearchForm.f.php';
 
 // Ensure a student is selected.
 if ( empty( $_REQUEST['student_id'] ) )
