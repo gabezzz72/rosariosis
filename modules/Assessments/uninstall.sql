87c5b47af0753fd1659217ce845e1270f5e750c3
gabezzz72 <gabez@mindaccel.org>
2025-11-15 13:04:32 -0600

    Create uninstall.sql

---
+++ modules/Assessments/uninstall.sql
@@ -0,0 +1,7 @@
+--
+-- SQL commands to uninstall the Assessments module
+-- Drops the tables created during installation.
+--
+
+DROP TABLE IF EXISTS assessment_scores;
+DROP TABLE IF EXISTS assessments;
