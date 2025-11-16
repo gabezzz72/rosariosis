1af4ea1d5d359e00a33c46520977ece28527f3c8
gabezzz72 <gabez@mindaccel.org>
2025-11-15 19:15:02 -0600

    Delete modules/Assessments directory

--- modules/Assessments/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$RosarioModules['Assessments'] = array(
-    'title' => _('Assessments'),
-    'category' => _('Students'),
-    'description' => _('Create assessment types and enter student assessment scores.'),
-    'version' => '1.0'
-);
--- modules/Assessments/functions.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function GetAssessmentTypes()
-{
-    return DBGet("SELECT ID, TITLE, SHORT_NAME FROM assessments_types ORDER BY SORT_ORDER");
-}
