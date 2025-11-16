cc230012c55332be12d06ef9eeabf08eafd061ed
gabezzz72 <gabez@mindaccel.org>
2025-11-15 19:34:56 -0600

    Update AssessmentTypes.php

--- modules/Student_Assessments/AssessmentTypes.php
+++ modules/Student_Assessments/AssessmentTypes.php
@@ -6,8 +6,8 @@
  * @subpackage modules
  */
 
-require_once 'ProgramFunctions/List.php';
-require_once 'ProgramFunctions/Update.php';
+require_once '../../ProgramFunctions/List.php';
+require_once '../../ProgramFunctions/Update.php';
 
 DrawHeader( _( 'Assessment Types' ) );
 
