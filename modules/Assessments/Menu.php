6d0e9efc050c9fc180ea762b0af80dfabbbc6719
gabezzz72 <gabez@mindaccel.org>
2025-11-15 18:20:44 -0600

    Update Menu.php

--- modules/Assessments/Menu.php
+++ modules/Assessments/Menu.php
@@ -1,15 +1,15 @@
 <?php
 
-// Add menu item under Students
-$_ROSARIO_MENU['Students']['Assessments'] = array(
+// Add Assessments under the Students menu
+$_ROSARIO_MENU['students']['Assessments'] = array(
     'title' => _('Assessments'),
     'link'  => 'modules/Assessments/Assessments.php',
     'search' => false
 );
 
-// Add menu item for Assessment Types (Admin only)
-if (User('PROFILE') == 'admin' || User('PROFILE_ID') == 1) {
-    $_ROSARIO_MENU['School_Setup']['Assessment Types'] = array(
+// Admin-only menu for assessment types
+if (User('PROFILE_ID') == 1) {
+    $_ROSARIO_MENU['school_setup']['Assessment Types'] = array(
         'title' => _('Assessment Types'),
         'link'  => 'modules/Assessments/AssessmentTypes.php',
         'search' => false
