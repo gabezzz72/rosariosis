63c2ef2833002bd5531d2a75a8f4a96ed8b6ea49
gabezzz72 <gabez@mindaccel.org>
2025-11-15 18:03:58 -0600

    Create config.inc.php

---
+++ modules/Assessments/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+$RosarioModules['Assessments'] = array(
+    'title' => _('Assessments'),
+    'category' => _('Students'),
+    'description' => _('Create assessment types and enter student assessment scores.'),
+    'version' => '1.0'
+);
