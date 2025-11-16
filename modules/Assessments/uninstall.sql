4dbf5969393dc78f9f25325e43c298807fe98da3
gabezzz72 <gabez@mindaccel.org>
2025-11-15 18:02:02 -0600

    Delete modules/Assessments directory

--- modules/Assessments/uninstall.sql
+++
@@ -1,7 +0,0 @@
---
--- SQL commands to uninstall the Assessments module
--- Drops the tables created during installation.
---
-
-DROP TABLE IF EXISTS assessment_scores;
-DROP TABLE IF EXISTS assessments;
